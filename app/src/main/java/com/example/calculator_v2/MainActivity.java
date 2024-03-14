fef38acecbb11e8da21d6a4cdd8b91d8cd9f24a6
Lior1254 <liorshemtov2008@gmail.com>
2024-03-14 22:37:48 +0200

    plus: work good no with =

--- app/src/main/java/com/example/calculator_v2/MainActivity.java
+++ app/src/main/java/com/example/calculator_v2/MainActivity.java
@@ -11,7 +11,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     EditText eD1;
     Button plus, minus,mult,div,AC,equal,credirs;
-    float num1;
+    float num1,num2,ans;
+    byte sign;// 1 = + , 2 = - , 3 = * , 4 = /
+    boolean fplus,fminus,fmult,fdiv;
+    char c1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +35,114 @@ public class MainActivity extends AppCompatActivity {
 
     public void lol(View view)
     {
+
+    }
+
+    public void plus(View view)
+    {
+        sign = 1;
+        String st = eD1.getText().toString();
+        if(!st.isEmpty())
+        {
+            if(fplus)
+            {
+                //1+2 =3+1
+
+                num2 = Integer.parseInt(st);
+                ans = ans +num2 + num1;
+                num1 = 0;
+                eD1.setHint(ans+"");
+            }
+            else
+            {
+                num1 = Integer.parseInt(st);
+            }
+            eD1.setText("");
+
+
+        }
+        fplus = true;
+
+    }
+
+    public void minus(View view)
+    {
+        sign = 2;
+        String st = eD1.getText().toString();
+        eD1.setText("");
+        if(!st.isEmpty())
+        {
+            num1 = Integer.parseInt(st);
+            eD1.setText("");
+
+
+        }
+    }
+
+    public void mult(View view)
+    {
+        sign = 3;
         String st = eD1.getText().toString();
         if(!st.isEmpty())
         {
             num1 = Integer.parseInt(st);
+            eD1.setText("");
+
+        }
+    }
+
+    public void div(View view)
+    {
+        sign = 4;
+        String st = eD1.getText().toString();
+        if(!st.isEmpty())
+        {
+            num1 = Integer.parseInt(st);
+            eD1.setText("");
+
+        }
+    }
+
+    public void equal(View view)
+    {
+        String st = eD1.getText().toString();
+        if(!st.isEmpty())
+        {
+            num2 = Integer.parseInt(st);
+            switch (sign)
+            {
+                case 1:
+                    num1 = num1 + num2;
+                    break;
+                case 2:
+                    num1 = num1 - num2;
+                    break;
+                case 3:
+                    num1 = num1 * num2;
+                    break;
+                    case 4:
+                    num1 = num1 / num2;
+                    break;
+
+            }
+            num2 = 0;
+            eD1.setText("");
+            eD1.setHint(num1+" ");
 
         }
         else
         {
             Toast.makeText(this, "You Need To Enter a Number", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void AC(View view)
+    {
+        num1 = 0;
+        num2 = 0;
+        sign = 0;
+        eD1.setText("");
+        eD1.setHint("");
+
     }
 }
